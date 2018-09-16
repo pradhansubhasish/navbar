6f5519b44ded020007680b873c20178c5db801cb
subhasish <subhasish.pradhan9002@gmail.com>
2018-09-16 17:24:12 +0530

    my-first-commit

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+
+    $(".dropdown").click(function(){
+        $(".dropdown-content").show("slow");    //dropdown content is shown....
+
+    });
+
+    
+});
+$(document).ready(function(){
+	
+
+   
+    $(".name").click(
+
+    	function(){ $(".dropdown-content").hide();  //dropdown content is hiden....
+    });
+
+
+    
+    
+});
+
